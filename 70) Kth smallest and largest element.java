5e99d34c0f0ef6c4ea3dfe32badc991e869dd032
himanshups <74396707+himanshups@users.noreply.github.com>
2023-06-27 19:50:10 +0530

    Add files via upload

---
+++ 70) Kth smallest and largest element.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Solution {
+	public static ArrayList<Integer> kthSmallLarge(ArrayList<Integer> arr, int n, int k) {
+		//Write your code here
+		Collections.sort(arr);
+		ArrayList<Integer> ans = new ArrayList<>(2);
+		ans.add(arr.get(k-1));
+		ans.add(arr.get(n-k));
+		return ans;
+	}
+}
