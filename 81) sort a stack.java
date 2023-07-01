5246548569f443fefeee99d78565bcf9c686aea7
himanshups <74396707+himanshups@users.noreply.github.com>
2023-07-01 22:49:12 +0530

    Add files via upload

---
+++ 81) sort a stack.java
@@ -0,0 +1,10 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+	public static void sortStack(Stack<Integer> stack) {
+		// Write your code here.
+		Collections.sort(stack);
+	}
+
+}
